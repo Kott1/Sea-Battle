16d2777b164be39a3ea848d30d2280f233bd32c0
Kott1 <mrkotto96@gmail.com>
2023-12-05 21:44:26 +0200

    Переробив файли ship

--- Sea_Battle_Game/Sea_Battle_Game/ship.h
+++ Sea_Battle_Game/Sea_Battle_Game/ship.h
@@ -5,25 +5,19 @@
 
 class Ship {
 private:
-    std::string type; // Тип корабля
-    int health; // Здоров'я корабля
-    int damage; // Шкода, яку може завдати корабель
-    // Можна додати інші характеристики
+    bool isAlive;
+    int health;
+    int damage;
+    int x_start, x_end;
+    int y_start, y_end;
 
 public:
-    Ship(std::string type, int health, int damage);
-
-    void takeDamage(int amount); // Функція для отримання шкоди
-    bool isSunk() const; // Перевірка, чи корабель потоплено
-    void attack(Ship& target); // Атака іншого корабля
-
-    // Геттери та сеттери для різних полів
-    std::string getType() const;
-    int getHealth() const;
-    int getDamage() const;
-    void setHealth(int newHealth);
-    // Інші методи...
+    void takeDamage(int health);
+    int getHealth();
+    bool getIsAlive();
+    int getPosition();
+    void setIsAlive();
 };
 
-#endif // SHIP_H
+#endif SHIP_H
 
