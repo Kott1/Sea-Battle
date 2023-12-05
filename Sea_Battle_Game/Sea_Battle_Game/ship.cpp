f91270855f59eabafce4b354d69e55657a647ea8
Kott1 <mrkotto96@gmail.com>
2023-12-05 21:44:26 +0200

    Переробив файли ship

--- Sea_Battle_Game/Sea_Battle_Game/ship.cpp
+++ Sea_Battle_Game/Sea_Battle_Game/ship.cpp
@@ -1,38 +1,21 @@
-#include "Ship.h"
+#include "ship.h"
 
-Ship::Ship(std::string type, int health, int damage)
-    : type(type), health(health), damage(damage) {}
-
-void Ship::takeDamage(int amount) {
-    health -= amount;
-    if (health < 0) {
-        health = 0;
-    }
+void Ship::takeDamage(int health) {
+    //
 }
 
-bool Ship::isSunk() const {
-    return health <= 0;
-}
-
-void Ship::attack(Ship& target) {
-    target.takeDamage(damage);
-}
-
-std::string Ship::getType() const {
-    return type;
-}
-
-int Ship::getHealth() const {
+int Ship::getHealth() {
     return health;
 }
 
-int Ship::getDamage() const {
-    return damage;
+bool Ship::getIsAlive() {
+    return isAlive ? true : false;
 }
 
-void Ship::setHealth(int newHealth) {
-    health = newHealth;
-    if (health < 0) {
-        health = 0;
-    }
+int Ship::getPosition() {
+    //
+}
+
+void Ship::setIsAlive() {
+    isAlive = false;
 }
