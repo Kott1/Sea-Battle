3c3708feb63a975deb749a0ce6f080a3c9d8f440
Sprite <patrondimon1290@gmail.com>
2024-01-09 22:29:59 +0200

    updates to battle logic & event manager

--- Sea_Battle_Game/Sea_Battle_Game/battle_logic.h
+++ Sea_Battle_Game/Sea_Battle_Game/battle_logic.h
@@ -7,18 +7,21 @@
 class Player
 {
 private:
-    std::vector<std::vector<int>> Field;
-    std::vector<BaseShip*> Ships;
+	int id;
 
-    bool CanPlace(Vec2 Pos, int size, bool isHorizontal);
+	std::vector<std::vector<int>> Field;
+	std::vector<BaseShip*> Ships;
+
+	bool CanPlace(Vec2 Pos, int size, bool isHorizontal);
 public:
-    const char* name;
+	const char* name;
+	int totalShipCount;
 
-    void PlaceShip(Vec2 Pos, bool IsHorizontal, BaseShip* NewShip);
-    BaseShip* HitCell(Vec2 Pos);
+	void PlaceShip(Vec2 Pos, bool IsHorizontal, BaseShip* NewShip);
+	BaseShip* HitCell(Vec2 Pos);
 
-    bool Shoot(Player* target, Vec2 Pos);
+	bool Shoot(Player* target, Vec2 Pos);
 
-    Player(int nid);
+	Player(int nid);
 };
 #endif
--- Sea_Battle_Game/Sea_Battle_Game/eventManager.h
+++ Sea_Battle_Game/Sea_Battle_Game/eventManager.h
@@ -1,16 +1,40 @@
 #ifndef EVENTMANAGER_H
 #define EVENTMANAGER_H
 
-#include "BaseShip.h"
+#include <vector>
+#include "baseShip.h"
+
+enum GameEvents : int {
+    OnGameStart = 0,
+    OnGameStop = 1,
+    OnShipHit = 2,
+    OnShipDestroy = 3,
+    OnPlayerWin = 4
+};
+
+struct OnPlayerWinContext {
+    int winerId;
+};
+
+struct OnShipHitContext
+{
+    int attackerId;
+    BaseShip* hittedShip;
+};
 
 class EventManager {
 private:
-	static EventManager* inst;
+    EventManager();
+    ~EventManager() {};
 
-	EventManager();
-	~EventManager() {};
+    std::vector<std::vector<void*>> eventHandlersList;
+    static EventManager* inst;
 public:
-	static EventManager* getSingleton();
+    static EventManager* getSingleton();
+
+    bool registerEvent(int id, void* func);
+    bool removeEvent(int id, void* func);
+    bool triggerEvent(int id, void* Context);
 };
 
 #endif
